e6531b297f3c4e610f793820d963cceefaacca45
jonathan james <yassermn238@gmail.com>
2024-11-24 17:21:58 -0800

    Deleted frontend and backend directories

--- backend/src/frontend/src/app.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("greet-button").addEventListener("click", function() { 
-document.getElementById("welcome-message").textContent = "Hello, User! Welcome to LinkedHub!"; 
-}); 
